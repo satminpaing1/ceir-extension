e00b1f924d5917fdc904b9806f5f15f1577ca84b
satminpaing1 <big680415@gmail.com>
2026-05-21 16:37:17 +0630

    Update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,15 +1,32 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'wxt';
+import obfuscator from 'vite-plugin-javascript-obfuscator';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   vite: () => ({
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      obfuscator({
+        include: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+        exclude: [/node_modules/],
+        apply: 'build', // GitHub ကနေ Zip ထုတ်တဲ့အချိန်မှသာ အလုပ်လုပ်စေရန်
+        options: {
+          compact: true,
+          controlFlowFlattening: true,
+          controlFlowFlatteningThreshold: 0.5,
+          identifierNamesGenerator: 'hexadecimal', 
+          stringArray: true,
+          stringArrayEncoding: ['base64'], 
+          splitStrings: true,
+          simplify: true
+        }
+      })
+    ]
   }),
   manifest: {
     host_permissions: ['https://ceir.gov.mm/check-status'],
-    // Extension အား Chrome Storage (မှတ်ဉာဏ်) အသုံးပြုခွင့်ပေးရန်
     permissions: ['storage'], 
   },
 });
