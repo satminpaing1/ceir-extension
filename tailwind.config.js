1729c69ad9f8e4ca35ef58c2c558f5dc6f7c3b8f
satminpaing1 <big680415@gmail.com>
2026-05-21 11:17:44 +0630

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./entrypoints/**/*.{html,ts,tsx}",
+    "./components/**/*.{html,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
